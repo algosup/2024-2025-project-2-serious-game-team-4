8f33a3d49c126ed5c172950087aab10d261903b0
Ianlaur <ian.laurent@algosup.com>
2024-11-27 15:54:34 +0100

    Revert "Merge pull request #11 from algosup/Emilien"
    This reverts commit 3555331222b9bee1a1a11d40246aabce69d283c3, reversing
    changes made to abc2ad969b06a8a4e24eda624ec8698805d7fd7f.

--- GAME/Scripts/UI/Minimap.gd
+++
@@ -1,7 +0,0 @@
-extends SubViewport
-
-@onready var camera = $Camera2D
-
-# sets the minimaps position to the player
-func _physics_process(delta: float) -> void:
-	camera.position = owner.find_child("Player").position
