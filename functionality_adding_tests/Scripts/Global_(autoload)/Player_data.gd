2fde32997a04e5acc86e72ae70bfd03da7bc1271
EmilienChinsy <emilien.chinsy@algosup.com>
2024-11-27 12:23:51 +0100

    added the main menu,
    This is a version I didn't want to break as it's the one that works when I upload it to itch.io

---
+++ functionality_adding_tests/Scripts/Global_(autoload)/Player_data.gd
@@ -0,0 +1,21 @@
+extends Node
+
+# This is a singleton to store the players position
+
+#Player position
+var player_pos : Vector2 = Vector2.ZERO
+#Player rotation
+var player_rot : String = ""
+
+func set_position(pos: Vector2):
+	player_pos = pos
+
+func set_rotation(rot : String):
+	player_rot = rot
+	print(player_rot)
+
+func get_position():
+	return player_pos
+
+func get_rotation():
+	return player_rot
