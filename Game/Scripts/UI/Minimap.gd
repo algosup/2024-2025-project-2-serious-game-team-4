fa6004a712ddf3fab260e4acd961e9d7b6a8b51b
Ianlaur <ian.laurent@algosup.com>
2024-11-29 15:18:32 +0100

    update

--- Game/Scripts/UI/Minimap.gd
+++ Game/Scripts/UI/Minimap.gd
@@ -3,5 +3,5 @@ extends SubViewport
 @onready var camera = $Camera2D
 
 # sets the minimaps position to the player
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta: float) -> void:
 	camera.position = owner.find_child("Player").position
