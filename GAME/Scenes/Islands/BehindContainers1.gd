9458c56ccb4f5cde2f5e5de73fd18f57c6efa4bf
Ianlaur <ian.laurent@algosup.com>
2024-12-06 15:24:23 +0100

    Update on GAME
    Containers transparency issue fixed

---
+++ GAME/Scenes/Islands/BehindContainers1.gd
@@ -0,0 +1,19 @@
+extends Area2D
+@onready var Building = $"../Container1"
+@onready var animated_sprite = $"../../Player"
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group('Player'):
+		Building.modulate = Color(0.777, 0.777, 0.777, 0.7)
+		animated_sprite.z_index = -1
+		pass
+
+func _on_body_exited(body: Node2D) -> void:
+	if body.is_in_group('Player'):
+		Building.modulate = Color(0.77, 0.77, 0.77)
+		animated_sprite.z_index = 0
+		pass
--- GAME/Scenes/Islands/Water.gd
+++ GAME/Scenes/Islands/Water.gd
@@ -8,25 +8,24 @@ var loops = 0
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
-	
+
 func _process(delta: float) -> void:
+	if loops == 3:
+		loops = 0
+	else:
+		loops += 1
+#	water_managemednt_function()
+
+func water_management_function(loops):
 	if loops == 0 :
-		loops = 1
 		Water1.visible = true
 		Water2.visible = false
 		Water3.visible = false
-
 	if loops == 1 :
 		Water1.visible = false
 		Water2.visible = true
 		Water3.visible = false
-		loops = 2
-
 	if loops == 2 :
 		Water1.visible = false
 		Water2.visible = false
 		Water3.visible = true
-		loops = 3
-			
-	if loops == 3 :
-		loops == 0
