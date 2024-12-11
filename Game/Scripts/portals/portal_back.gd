83e229d5b05e8cf48b35f3f2038e848c27210621
EmilienChinsy <emilien.chinsy@algosup.com>
2024-12-12 00:05:45 +0100

    Fixed the issue with the Island2_2 portals

--- Game/Scripts/portals/portal_back.gd
+++ Game/Scripts/portals/portal_back.gd
@@ -12,11 +12,13 @@ var on_portal = false
 func _on_body_entered(body):
 	if body.is_in_group('Player'):
 		on_portal = true
+		body.Interact_UI.visible = true
 		if not interact:
 			portal_entered.emit(destination, main_name_destination, new_pos)
 
 func _on_body_exited(body: Node2D) -> void:
 	if body.is_in_group('Player'):
+		body.Interact_UI.visible = false
 		on_portal = false
 
 func _input(event):
