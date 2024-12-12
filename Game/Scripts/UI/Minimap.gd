4ea8be292dc8fe077a07947f99e27de5e018f785
Ianlaur <146005340+Ianlaur@users.noreply.github.com>
2024-12-12 16:11:28 +0100

    Delete Game directory

--- Game/Scripts/UI/Minimap.gd
+++
@@ -1,7 +0,0 @@
-extends SubViewport
-
-@onready var camera = $Camera2D
-
-# sets the minimaps position to the player
-func _physics_process(_delta: float) -> void:
-	camera.position = owner.find_child("Player").position
