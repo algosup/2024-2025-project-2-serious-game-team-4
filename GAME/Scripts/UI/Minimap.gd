1c214d9a8cfcd3a324f66d424b57ef18710d5a09
EmilienChinsy <emilien.chinsy@algosup.com>
2024-12-19 12:03:11 +0100

    You wanted a push, here you have it

--- GAME/Scripts/UI/Minimap.gd
+++ GAME/Scripts/UI/Minimap.gd
@@ -1,12 +1,19 @@
 extends SubViewport
 
+@onready var minimap = $Minimap
 @onready var camera = $Camera2D
 @onready var canvas_layer = $"../.."
+@onready var player = $"../../../Player"
 
-# sets the minimaps position to the player
-func _physics_process(_delta: float) -> void:
-	camera.position = owner.find_child("Player").position
+func _ready() -> void:
+	camera.make_current()
+	camera.global_position = player.global_position
+
+
+func _process(delta):
+	camera.global_position = player.global_position
 
 #it starts invisible so that the tutorial isn't overlapped by the minimap
 func _on_player_tuto_done(is_done: Variant) -> void:
 	canvas_layer.visible = is_done
+	
