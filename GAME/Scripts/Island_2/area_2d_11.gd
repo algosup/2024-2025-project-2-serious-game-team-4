5ebb3952186c128993c09247dc90556999000bd6
Ianlaur <ian.laurent@algosup.com>
2024-12-19 09:14:20 +0100

    Update on GAME

--- GAME/Scripts/Island_2/area_2d_11.gd
+++ GAME/Scripts/Island_2/area_2d_11.gd
@@ -8,11 +8,13 @@ func _on_area_2d_body_entered_behind_building(body: Node2D) -> void:
 	if body.is_in_group('Player'):
 		Building.modulate = Color(0.77,0.74,0.69,0.8)
 		Building2.modulate = Color(0.77,0.74,0.69,0.8)
-		animated_sprite.z_index = -1
+		Building.z_index = +1
+		Building2.z_index = +1
 
 #This reverts the opaqueness
 func _on_area_2d_body_exited_behind_building(body: Node2D) -> void:
 	if body.is_in_group('Player'):
 		Building.modulate = Color(0.77,0.74,0.68,1)
 		Building2.modulate = Color(0.77,0.74,0.68,1)
-		animated_sprite.z_index = 0
+		Building.z_index = 0
+		Building2.z_index = 0
